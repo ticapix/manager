da30389b3e57f20017a243452476dd6ad922d252
Ravindra.Adireddy <raadired@LAPTOPC02SY20FGTFL.local>
2019-08-29 19:00:49 +0530

    added icon flag css library

--- packages/manager/modules/enterprise-cloud-database/src/create/choose-database/choose-database.constants.js
+++ packages/manager/modules/enterprise-cloud-database/src/create/choose-database/choose-database.constants.js
@@ -1,3 +1,10 @@
-import PG_SQL_ICON from '../../assets/pgsql.png';
+import PG_SQL from '../../assets/postgreSQL.svg';
+import MARIA_DB from '../../assets/mariadb.svg';
 
-export default PG_SQL_ICON;
+export const PG_SQL_ICON = PG_SQL;
+export const MARIA_DB_ICON = MARIA_DB;
+
+export default {
+  PG_SQL_ICON,
+  MARIA_DB_ICON,
+};
