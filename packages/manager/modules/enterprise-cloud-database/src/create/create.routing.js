2771aefec8fbf67483447bd7a1d0f7431cdda408
Ravindra.Adireddy <raadired@LAPTOPC02SY20FGTFL.local>
2019-09-11 16:42:28 +0530

    populate price for additional replicas

--- packages/manager/modules/enterprise-cloud-database/src/create/create.routing.js
+++ packages/manager/modules/enterprise-cloud-database/src/create/create.routing.js
@@ -2,6 +2,10 @@ export default /* @ngInject */($stateProvider) => {
   $stateProvider.state('enterprise-cloud-database.create', {
     component: 'enterpriseCloudDatabaseCreateComponent',
     url: '/create',
+    translations: {
+      value: ['.'],
+      format: 'json',
+    },
     resolve: {
       breadcrumb: /* @ngInject */ $translate => $translate.instant('enterprise_cloud_database_create_title'),
     },
--- packages/manager/modules/enterprise-cloud-database/src/price/price.controller.js
+++ packages/manager/modules/enterprise-cloud-database/src/price/price.controller.js
@@ -1,13 +1,17 @@
 export default class EnterpriseCloudDatabasePriceCtrl {
   /* @ngInject */
-  constructor(
-    enterpriseCloudDatabaseService,
-  ) {
-    this.service = enterpriseCloudDatabaseService;
+  constructor() {
     this.frenchTouch = ['CZ', 'ES', 'FR', 'GB', 'IE', 'IT', 'LT', 'MA', 'NL', 'PL', 'PT', 'TN'];
     this.deutchTouch = ['DE', 'FI', 'SN'];
     this.usTouch = ['CA', 'WE', 'WS', 'QC', 'US'];
     this.asiaTouch = ['SG', 'ASIA', 'AU'];
-    this.ovhSubsidiary = this.service.userData.ovhSubsidiary;
+  }
+
+  $onInit() {
+    this.ovhSubsidiary = this.user.ovhSubsidiary;
+  }
+
+  getPriceText(priceInCents) {
+    return `${priceInCents / 100000000} ${this.user.currency.symbol}`;
   }
 }
