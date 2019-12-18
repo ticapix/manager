2c008cd45397aabd2d40168f13020c6678e3d42a
Frederic Espiau <frederic.espiau@corp.ovh.com>
2019-12-18 11:30:08 +0100

    refactor(web): use global component instead of scoped one
    Signed-off-by: Frederic Espiau <frederic.espiau@corp.ovh.com>
    Co-authored-by: Jérémy De-Cesare <jeremy.de-cesare@corp.ovh.com>
    Co-authored-by: Axel Peter <axel.peter@corp.ovh.com>

--- packages/manager/apps/web/client/app/app.js
+++ packages/manager/apps/web/client/app/app.js
@@ -11,6 +11,7 @@ import ovhManagerCore from '@ovh-ux/manager-core';
 import ngAtInternet from '@ovh-ux/ng-at-internet';
 import ngAtInternetUiRouterPlugin from '@ovh-ux/ng-at-internet-ui-router-plugin';
 import ngOvhApiWrappers from '@ovh-ux/ng-ovh-api-wrappers';
+import ngOvhContracts from '@ovh-ux/ng-ovh-contracts';
 // import ngOvhChatbot from '@ovh-ux/ng-ovh-chatbot';
 import ngOvhExportCsv from '@ovh-ux/ng-ovh-export-csv';
 import ngOvhHttp from '@ovh-ux/ng-ovh-http';
@@ -39,7 +40,6 @@ import moment from 'moment';
 
 import config from './config/config';
 import orderCatalogPrice from './components/manager-order-catalog-price';
-import orderContracts from './components/manager-order-contracts';
 import orderService from './components/manager-order-service/manager-order-service.service';
 
 import domain from './domain';
@@ -77,6 +77,7 @@ angular
     ngAtInternet,
     ngAtInternetUiRouterPlugin,
     ngOvhApiWrappers,
+    ngOvhContracts,
     // ngOvhChatbot,
     ngOvhExportCsv,
     ngOvhHttp,
@@ -110,7 +111,6 @@ angular
     errorPage,
     hosting,
     orderCatalogPrice,
-    orderContracts,
     privateDatabase,
     zone,
   ])
